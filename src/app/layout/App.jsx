4addd7e8f54df8ec23bf254839338e85111f898a
bless4 <36552429+bless4@users.noreply.github.com>
2019-03-09 17:35:18 +0100

    create a folder inside src name app & create another folder name  layout inside app folder & installed all dependency & create a file name App.jsx in the layout folde

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Events Board</h1>
+        <p>Hello world</p>
+      </div>
+    );
+  }
+}
+
+export default App;
