b80e3262b0348ecd6590f22e4cdc25f4e668f33a
FamNew <thenewcomerfamily@msn.com>
2019-03-23 00:40:18 +0300

    added delete button and made delete and view work

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -8,7 +8,7 @@ class EventList extends Component {
       <div>
         <h1>Event List</h1>
         {events.map((event) => (
-          <EventListItem key={event.id} event={event}/>
+          <EventListItem key={event.id} event={event} deleteEvent={this.props.deleteEvent} onEventOpen={this.props.onEventOpen}/>
         ))}
       </div>
     )
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -36,7 +36,8 @@ class EventListItem extends Component {
         </Segment>
         <Segment clearing>
         <span>{event.description}</span>
-          <Button as="a" color="teal" floated="right" content="View" />
+          <Button onClick={this.props.deleteEvent(event.id)} as="a" color="red" floated="right" content="Delete" />  
+          <Button onClick={this.props.onEventOpen(event)} as="a" color="teal" floated="right" content="View" />
         </Segment>
       </Segment.Group>
     );
