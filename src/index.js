276658dc8bf63d13033af8511eaf55ab18c138a2
bless4 <36552429+bless4@users.noreply.github.com>
2019-04-03 12:03:52 +0200

    event

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
-import {configureStore} from './app/store/configureStore';
+import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/common/util/ScrollToTop';
+import { loadEvents } from './features/event/eventActions'
 
 const store = configureStore();
+store.dispatch(loadEvents())
+
 const rootEl = document.getElementById('root');
 
 let render = () => {
   ReactDOM.render(
     <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
+      </BrowserRouter>
     </Provider>,
     rootEl
   );
